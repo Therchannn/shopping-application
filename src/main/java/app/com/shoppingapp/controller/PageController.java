67d6f48c8cc7eb4a5c27eeb4255510faf415a86d
gnasche.tntt <trannguyenthanhtruc050@gmail.com>
2025-10-26 23:43:07 +0700

    header và footer để nhúng

--- src/main/java/app/com/shoppingapp/controller/PageController.java
+++ src/main/java/app/com/shoppingapp/controller/PageController.java
@@ -1,13 +1,14 @@
 package app.com.shoppingapp.controller;
 
-import app.com.shoppingapp.dto.ProductDTO;
-import app.com.shoppingapp.service.ProductService;
-import lombok.RequiredArgsConstructor;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.List;
+import app.com.shoppingapp.dto.ProductDTO;
+import app.com.shoppingapp.service.ProductService;
+import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Controller
@@ -16,10 +17,27 @@ public class PageController {
 
     @GetMapping("/home")
     public String homePage(Model model){
+        // List<ProductDTO> products = productService.get();
+
+        // model.addAttribute("products", products);
+
+        return "home";
+    }
+
+    @GetMapping("/layout")
+    public String layoutPage(){
+        return "layout";
+    }
+
+    @GetMapping("/layoutProduct")
+    public String layoutProductPage(Model model){
+
         List<ProductDTO> products = productService.get();
 
         model.addAttribute("products", products);
 
-        return "home";
+        return "layoutProduct";
     }
+
+    
 }
